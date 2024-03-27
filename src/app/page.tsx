c6ec9b7f8df99aafbeabd82e86e3ddfff5f32343
karabulut <mehmetcan.karabulut@outlook.com.tr>
2024-03-27 11:44:25 +0300

    items modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="flex h-32 shrink-0 items-end rounded-lg bg-blue-300 p-4">
-        
+      <div className="flex h-32 shrink-0 items-center justify-center rounded-lg bg-blue-300 p-4">
+        {/* LOGO */}
+        {/* SEARCH FİELD */}
+        {/* ACCOUNT BAR */}
+        {/* CART */}
+        <button type="submit">logo</button>
+        <button type="submit">search</button>
+        <button type="submit">account</button>
+        <button type="submit">cart</button>
       </div>
-      <div className="flex mt-4 grow flex-col gap-4 bg-orange-200">
+      <div className="flex mt-4 grow flex-col gap-4 bg-white-200">
 
       </div>
     </main>
