d93eca2fb226a35fbb80a11b57a84a6e9f6b8548
mehmet can <mehmetcan.karabulut@outlook.com.tr>
2024-03-27 05:32:45 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Page() {
+  return (
+    <div className="w-screen h-screen">
+      <div className="top-side bg-blue-300 w-screen h-1/6">
+        
+      </div>
+      <div className="bottom-side bg-orange-200 h-full">
+
+      </div>
+    </div>
+  );
+}
