58e19708740a274480e3dfdf2daabdef9b7747cc
karabulut <mehmetcan.karabulut@outlook.com.tr>
2024-03-28 09:39:12 +0300

    temp data source for now

---
+++ src/lib/temp/placeholder-data.js
@@ -0,0 +1,15 @@
+
+
+const products = [
+    {
+        id: string,
+        name: string,
+        price: number,
+        image_url: string,
+        owner: string
+    }
+];
+
+module.exports = { 
+    products
+};
