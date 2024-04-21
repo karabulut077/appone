b455febdcbcb8d75d188aff6ef822b278c55c4c6
mehmet can <mehmetcan.karabulut@outlook.com.tr>
2024-04-21 15:01:43 +0300

    product details page added

--- src/lib/components/InitialProducts.jsx
+++ src/lib/components/InitialProducts.jsx
@@ -1,6 +1,7 @@
 
-import { parseProducts } from "../data";
-import Product from "./Product";
+import Link from "next/link";
+import { parseProducts } from "@/lib/data";
+import Product from "@/lib/components/Product";
 
 export default async function InitialProducts() {
     const [
@@ -16,10 +17,12 @@ export default async function InitialProducts() {
                 <div className="flex items-center justify-center">
                     <b>elektronik</b>
                 </div>
-                <div className="flex flex-row items-center justify-center">
+                <div className="flex flex-row items-center justify-center gap-4">
                     {
                         electronic_data.map(product =>
-                            <Product key={product.id} product={product} />
+                            <Link key={product.id} href={`/product/${product.id}`}>
+                                <Product key={product.id} product={product} />
+                            </Link>
                         )
                     }
                 </div>
@@ -28,10 +31,12 @@ export default async function InitialProducts() {
                 <div className="flex items-center justify-center">
                     <b>giyim</b>
                 </div>
-                <div className="flex flex-row items-center justify-center">
+                <div className="flex flex-row items-center justify-center gap-4">
                     {
                         clothing_data.map(product =>
-                            <Product key={product.id} product={product} />
+                            <Link key={product.id} href={`/product/${product.id}`}>
+                                <Product key={product.id} product={product} />
+                            </Link>
                         )
                     }
                 </div>
@@ -40,10 +45,12 @@ export default async function InitialProducts() {
                 <div className="flex items-center justify-center">
                     <b>kozmetik</b>
                 </div>
-                <div className="flex flex-row items-center justify-center">
+                <div className="flex flex-row items-center justify-center gap-4">
                     {
                         cosmetic_data.map(product =>
-                            <Product key={product.id} product={product} />
+                            <Link key={product.id} href={`/product/${product.id}`}>
+                                <Product key={product.id} product={product} />
+                            </Link>
                         )
                     }
                 </div>
@@ -52,10 +59,12 @@ export default async function InitialProducts() {
                 <div className="flex items-center justify-center">
                     <b>market</b>
                 </div>
-                <div className="flex flex-row items-center justify-center">
+                <div className="flex flex-row items-center justify-center gap-4">
                     {
                         market_data.map(product =>
-                            <Product key={product.id} product={product} />
+                            <Link key={product.id} href={`/product/${product.id}`}>
+                                <Product key={product.id} product={product} />
+                            </Link>
                         )
                     }
                 </div>
