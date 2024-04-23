11b351cc2859e834f56e31b67f0ef12b4243c928
mehmet can <mehmetcan.karabulut@outlook.com.tr>
2024-04-23 11:05:49 +0300

    ui updated

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -8,13 +8,13 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <main className="flex min-h-screen flex-col">
       <div className="flex flex-col rounded-lg p-4">
-        <div className="flex flex-row bg-blue-300 space-x-12 h-32 shrink-0 items-center justify-center">
+        <div className="flex flex-row space-x-12 h-32 shrink-0 items-center justify-center bg-blue-300">
           <label>logo</label>
           <input type="search" placeholder="ürün arayın" />
           <button type="submit">hesabım</button>
           <button type="submit">sepetim</button>
         </div>
-        <div className="flex flex-row bg-red-300 space-x-12 justify-center">
+        <div className="flex flex-row space-x-12 justify-center bg-red-300">
           <label>elektronik</label>
           <label>giyim</label>
           <label>kozmetik</label>
