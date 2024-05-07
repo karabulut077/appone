74d941d3639286c0bf99ca802b6040182290d4da
mehmet can <mehmetcan.karabulut@outlook.com.tr>
2024-05-07 20:37:37 +0300

    tmp1

--- src/lib/components/InitialProducts.tsx
+++ src/lib/components/InitialProducts.tsx
@@ -115,12 +115,6 @@ function parseInitialProducts(initialProducts: ProductType[]) {
 async function writeDataToLocalFile(fileName: string, initialProducts: ProductType[]) {
     const filePath = path.join('/tmp', fileName);
 
-    fs.unlink(filePath).then(() => {
-        console.log(filePath, 'was deleted');
-    }).then(err => {
-        console.error('Error removing local file:', err);
-    });
-
     await fs.writeFile(filePath, JSON.stringify(initialProducts, null, 4), 'utf8')
         .then(() => {
             console.log('InitialProducts written to local file successfully.', filePath);
