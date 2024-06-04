048a2faa7b3dc3cadbe13ba0a834cb5ec36801fe
karabulut <mehmetcan.karabulut@outlook.com.tr>
2024-06-04 10:47:31 +0300

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-              protocol: process.env.IMG_PROTOCOL,
-              hostname: process.env.IMG_DOMAIN
+              protocol: process.env.IMG_PROTOCOL_D,
+              hostname: process.env.IMG_DOMAIN_D
             }
         ]
     },
     env: {
-        SERVER_IP: process.env.SERVER_IP,
-        SERVER_PORT : process.env.SERVER_PORT
+        SERVER_IP: process.env.SERVER_IP_D,
+        SERVER_PORT : process.env.SERVER_PORT_D
     }
 };
 
