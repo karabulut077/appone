40c77f55ed3f82bc8acde01a1f90ecbdeb9bffda
karabulut <mehmetcan.karabulut@outlook.com.tr>
2024-06-04 16:27:59 +0300

    common .env variables

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-              protocol: process.env.IMG_PROTOCOL_D,
-              hostname: process.env.IMG_DOMAIN_D
+              protocol: process.env.IMG_PROTOCOL,
+              hostname: process.env.IMG_DOMAIN
             }
         ]
     },
     env: {
-        SERVER_IP: process.env.SERVER_IP_D,
-        SERVER_PORT : process.env.SERVER_PORT_D
+        SERVER_IP: process.env.SERVER_IP,
+        SERVER_PORT : process.env.SERVER_PORT
     }
 };
 
